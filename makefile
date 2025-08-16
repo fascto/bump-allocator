1c96c987b4c806002fadb4f7ff62d5c24ae0b6bd
fascto <fascto.05@gmail.com>
2025-08-16 12:39:37 -0300

    adding the structs, types and some prototypes

--- makefile
+++ makefile
@@ -1,6 +1,5 @@
 
 # Especifico que el compilador que voy a usar es gcc
-CC = gcc
 
 # Las flags que se le van a pasar al compilador
 
@@ -14,28 +13,24 @@ CC = gcc
 
 # -g compila con la informacion de debug
 
+CC = gcc
 CFLAGS = -Wall -Wextra -Iinclude -std=c99 -O0 -g
-
-# Directorio donde tengo el codigo
 SRC_DIR = src
-
-
-SOURCES = $(wildcard $(SRC_DIR)/*.c)
-
 TARGET_DIR = target
 
+SOURCES = $(wildcard $(SRC_DIR)/*.c)
 OBJECTS = $(patsubst $(SRC_DIR)/%.c,$(TARGET_DIR)/%.o,$(SOURCES))
-
 TARGET = $(TARGET_DIR)/target
 
 all: $(TARGET)
 
 $(TARGET_DIR)/%.o: $(SRC_DIR)/%.c
-    @mkdir -p $(TARGET_DIR)
-    $(CC) $(CFLAGS) -c $< -o $@
+	@mkdir -p $(TARGET_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
 
-# PHONY indica que clean siempre va a la regla se ejecute siempre y evita conflictos con archivos que tengan el mismo nombre que el target.
-.PHONY: clean3
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $^ -o $@
 
+.PHONY: clean
 clean:
-    @rm -f $(TARGET) $(OBJECTS) core
+	@rm -f $(TARGET) $(OBJECTS) core
--- src/main.c
+++ src/main.c
@@ -1,32 +1,84 @@
+#define _DEFAULT_SOURCE
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
 
-#define R 10
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                          SEGMENTOS DE MEMORIA DE UN PROCESO                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Primero un poco de contexto, el stack es un estructura de datos estatica con un tamaño definido. 
-// Las direcciones bajas de memoria text 
+// Las direcciones bajas de memoria a la mas alta:
 
-struct bump_allocator
-{
+// TEXT: se le dice le "code segment" o "text segment", es donde se guardan las instrucciones de del programa. 
+// Tiene el codigo ejecutable, las funciones, declaraciones y demas cuestiones para que el programa funque.
+// Este pedazo de memoria es de solo lectura y se comparte con demas partes del programa.
 
-} bump_allocator_t;
+// DATA SEGMENT: luego del text segment le sigue el data segment el cual se divide en 2, "initialized data segment" (.data) y 
+// "uninitilized data" (.bss)
 
+//// el primer segmento almacena las variables globales y estaticas inicializadas (que tiene un valor explicito asignados).
+//// Osea que cuando inicializamos una variable, se esta reservando y posteriormente asignando en esta parte de la memoria. (tiempo de compilacion).
+
+//// Luego tenemos el otro segmento que no esta inicializada de manera explicita variables globales y estaticas. Ejemplo: 
+//// int global; ó int static elpepe;
+
+// STACK: El STACK es un segmento de memoria que esta en las direcciones mas alta de memoria y que va a crecer mientras se hacen llamada de funciones.
+// Lo que se crea es un stack frame, el cual tiene las variables locales, la direccion de retorno (para saber donde termina la funcion),
+// los parametros de la funcion si los tiene.
+
+// Por ultimo esta el HEAP que es donde vamos a alojar memoria de manera dinamica con algun allocator como lo es malloc. LO que nos indica
+// que esta memoria puede ser accedida mediante una referencia usando punteros. Esto es lo que vamos a intentar "replicar". 
+
+
+typedef struct header header_t;
+typedef struct content content_t;
+
+typedef struct memory_block memory_block_t;
+
+struct header {
+    size_t size;
+    int *next;
+    bool is_free;
+};
+
+struct content {
+    char *data;
+};
+
+struct memory_block {
+    header_t header;
+    content_t *content;
+};
 
 // funcion para pedir memoria
-void *alloc(size_t ) {
-
+void *alloc(size_t bytes_to_alloc) {
+    // TODO
+    return NULL;
 }
 
 
 // funcion para devolver memoria
 void dealloc(void *mem) {
+    // TODO
+}
 
+void align() {
+    // TODO
 }
 
 int main(void) {
 
+    // BRK -> Me va a tirar la direccion de donde arranca el heap
+
+    void *heap = sbrk(0);
+    // La direccion de memoria donde me dejo;
+
+    printf("HEAP: 0x%lx\n", (uintptr_t)heap);
 
     return 0;
 
